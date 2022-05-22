7c69ae8b0662edd5be0d1809b09ec24dd75837e1
antijun <68279777+antijun@users.noreply.github.com>
2022-05-21 23:35:49 -0700

    update

--- ui.py
+++ ui.py
@@ -2,4 +2,20 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets, uic
 
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('form.ui', self)
+        self.show()
+        self.pushButton.clicked.connect(self.clickme)
+
+    def clickme(self):
+        print("pressed")
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
