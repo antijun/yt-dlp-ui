547fa7748cb002bc038af67ddd6d3b2a4284cbce
antijun <68279777+antijun@users.noreply.github.com>
2022-05-24 04:35:26 -0700

    update

--- ui.py
+++ ui.py
@@ -15,15 +15,20 @@ class Ui(QtWidgets.QMainWindow):
         self.downloadButton.clicked.connect(self.startDownload)
         self.videoOnlyCheck.toggled.connect(self.video_exclusiveCheck)
         self.audioOnlyCheck.toggled.connect(self.audio_exclusiveCheck)
+        self.setDownload.clicked.connect(self.set_downloadLocation)
 
     def getLink(self):
-        link = self.enterLink.toPlainText()
+        link = self.enterLink.text()
         return link
 
     def startDownload(self):
         URL = self.getLink()
-        with YoutubeDL(self.getOptions()) as ydl:
+        with YoutubeDL(self.getOptions(self.set_downloadLocation())) as ydl:
             ydl.download(URL)
+
+    def set_downloadLocation(self):
+        location = 'downloads/%(title)s.%(ext)s'
+        return location
 
     def video_exclusiveCheck(self):
         if self.videoOnlyCheck.isChecked() == True:
@@ -33,21 +38,24 @@ class Ui(QtWidgets.QMainWindow):
         if self.audioOnlyCheck.isChecked() == True:
             self.videoOnlyCheck.setChecked(False)
 
-    def getOptions(self):
+    def getOptions(self, location):
         if self.audioOnlyCheck.isChecked() == True:
             ydl_opts = {
                 'format': 'bestaudio/best',
+                'outtmpl': location,
                 'extractaudio': True,
                 'audioformat': "mp3",
                 'noplaylist': True
             }
+            return ydl_opts
         elif self.videoOnlyCheck.isChecked() == True:
             ydl_opts = {
                 'format': 'bestvideo/best',
+                'outtmpl': location,
                 'videoformat': "mp4",
                 'noplaylist': True
             }
-        return ydl_opts
+            return ydl_opts
 
 
 app = QtWidgets.QApplication(sys.argv)
